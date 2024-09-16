922b992b556729deda656e043592b22175c7276d
Shruti Priya <111532652+sapphicart@users.noreply.github.com>
2024-09-16 22:28:24 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-      <main className="flex flex-col min-h-screen items-center justify-center">
+      <main className="flex flex-col h-screen items-center justify-center">
         <Hero />
       </main>
   );
