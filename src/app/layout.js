c87f67e05785f93b6a85f7fd29742ad1de79b2e6
Shruti Priya <111532652+sapphicart@users.noreply.github.com>
2024-09-17 14:50:30 +0530

    adding opengraph metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,20 @@ import Navbar from "./components/Navbar";
 export const metadata = {
   title: "Inkblot Book Club",
   description: "Join our book club where we read between the lines.",
+  openGraph: {
+      title: "Inkblot Book Club",
+      description: "Join our book club where we read between the lines.",
+      url: 'https://inkblot-book-club.vercel.app',
+      images: [
+        {
+          url: 'https://inkblot-book-club.vercel.app/cover.png',
+          width: 1200,
+          height: 630,
+          alt: "Cover image for Inkblot Book Club",
+        }
+      ],
+      type: 'website',
+  }
 }
 
 export default function RootLayout({ children }) {
